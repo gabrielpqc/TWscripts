6f125d3f189d46dd3b5cf416fd417604997933c0
Gabriel Charrua <gabrielpqcharrua@gmail.com>
2026-04-24 16:32:15 +0200

    Update new-barbarians-warning.js

--- new-barbarians-warning.js
+++ new-barbarians-warning.js
@@ -2,7 +2,7 @@
 // @name         Joe - TW Barbarian Finder
 // @author       Average Joe
 // @namespace    tribalwars.barbs
-// @version      1.6
+// @version      1.7
 // @description  Find barbarian villages in a 30-field radius, store in localStorage, and popup new ones
 // @match        *://*/game.php*?*screen=map*
 // @require      https://raw.githubusercontent.com/gabrielpqc/TWscripts/refs/heads/main/src/new-barbarians-warning/new-barbarians-warning.js
