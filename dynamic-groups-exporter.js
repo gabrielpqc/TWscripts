60260bff0abc8d091c14fed9189a29cd9e39a969
Gabriel Charrua <gabrielpqcharrua@gmail.com>
2026-04-24 14:09:16 +0200

    Create dynamic-groups-exporter.js

---
+++ dynamic-groups-exporter.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Joe - Dynamic Group Export/Import
+// @author       Average Joe
+// @version      1.0
+// @description  Export/import Tribal Wars dynamic group filter configs
+// @match        *://*/game.php*?*&type=dynamic
+// @require      https://raw.githubusercontent.com/gabrielpqc/TWscripts/refs/heads/main/dynamic%20groups%20exporter/dynamic-groups-exporter.js
+// @icon         https://raw.githubusercontent.com/gabrielpqc/TWscripts/refs/heads/main/icon.webp
+// ==/UserScript==
